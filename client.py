ce89702607d50779052ad01843fa834b0518f0f9
Valerii Pokrytiuk <pokrytiuk.valerii@steelkiwi.com>
2020-11-09 23:05:07 +0200

    Update client

--- client.py
+++ client.py
@@ -6,25 +6,26 @@ import requests
 class Nemesis:
     BASE_API_URL = 'http://localhost:8000'
 
-    def __init__(self, color='red', base_url=None):
+    def __init__(self, color='red', api_url=None):
         self.color = color
-        self.base_url = base_url or self.BASE_API_URL
+        self.api_url = api_url or self.BASE_API_URL
+        self.database = {}
 
     def enemies(self, only_my=True):
-        url = f'{self.base_url}/enemies/'
+        url = f'{self.api_url}/enemies/'
         if only_my:
             url += f'?color={self.color}'
         return requests.get(url).json()
 
     def scan_enemy(self, enemy_id):
-        return requests.get(self.base_url + f'/enemies/{enemy_id}/').json()
+        return requests.get(self.api_url + f'/enemies/{enemy_id}/').json()
 
     def kill(self, enemy_id, answer):
-        reponse = requests.post(
-            self.base_url + f'/enemies/{enemy_id}/kill/',
+        response = requests.post(
+            self.api_url + f'/enemies/{enemy_id}/kill/',
             json={'nemesis': json.dumps(answer)},
         )
-        print(reponse.json())
+        print(response.json())
             
     def connect(self):
-        print(requests.get(self.base_url + f'/connect/').text)
+        print(requests.get(self.api_url + f'/connect/').text)
