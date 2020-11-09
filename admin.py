29d91d28996a5b36239b7329c40143ba61544f67
Valerii Pokrytiuk <pokrytiuk.valerii@steelkiwi.com>
2020-11-09 23:31:36 +0200

    Rewrite admin

--- admin.py
+++ admin.py
@@ -10,32 +10,7 @@ from redis import Redis
 from serializers import EnemySchema
 
 
-ZERG_BREEDS = [
-   'Zergling',
-   # 'Baneling',
-   'Roach',
-   'Ravager',
-   'Hydralisk',
-   # 'Ultralisk',
-   'Queen',
-]
-
-COMPLEXITY_TO_BREED = {
-    0: ['Zergling'],
-    1: ['Roach'],
-    2: ['Hydralisk'],
-    3: ['Ravager']
-}
-
-TASKS_LIST = []
-for name, obj in inspect.getmembers(tasks):
-    if inspect.isclass(obj) and issubclass(obj, tasks.Task) and name != 'Task':
-        TASKS_LIST.append(obj)
-
-
 redis = Redis(host='localhost', port=6379, db=0, decode_responses=True)
-redis_listener = redis.pubsub(ignore_subscribe_messages=True)
-redis_listener.subscribe('game-commands')
 
 
 class ID:
@@ -47,19 +22,56 @@ class ID:
         return cls.id
 
 
-def spawn_wave(complexity, players_number, enemies):
-    player_colors = [
+class AdminService:
+    PLAYER_COLORS = [
         'red',
         'green',
         'blue',
     ]
 
-    def _spawn_enemy(color):
-        allowed_tasks = [task for task in TASKS_LIST if task.complexity <= complexity]
+    ZERG_BREEDS = [
+       'Zergling',
+       # 'Baneling',
+       'Roach',
+       'Ravager',
+       'Hydralisk',
+       'Queen',
+        # 'Ultralisk',
+    ]
+
+    COMPLEXITY_TO_BREED = {
+        0: ['Zergling'],
+        1: ['Roach'],
+        2: ['Hydralisk'],
+        3: ['Ravager']
+    }
+
+    def __init__(self):
+        self.tasks_list = []
+        for name, obj in inspect.getmembers(tasks):
+            if inspect.isclass(obj) and issubclass(obj, tasks.Task) and name != 'Task':
+                self.tasks_list.append(obj)
+
+    def spawn_wave(self, complexity, players_number, enemies):
+        # Spawn starting enemies
+        for color in self.PLAYER_COLORS[:players_number]:
+            for _ in range(enemies):
+                self.spawn_enemy(color, complexity)
+
+        # Sustain wave
+        while True:
+            enemies = [EnemySchema().loads(redis.get(key)) for key in redis.scan_iter("enemy:*")]
+            for color in self.PLAYER_COLORS[:players_number]:
+                if not filter(lambda enemy: enemy['color'] == color, enemies):
+                    self.spawn_enemy(color, complexity)
+            sleep(1)
+
+    def spawn_enemy(self, color, complexity):
+        allowed_tasks = [task for task in self.tasks_list if task.complexity <= complexity]
         task = random.choice(allowed_tasks)()
         enemy = {
             'id': ID.next(),
-            'breed': random.choice(COMPLEXITY_TO_BREED[task.complexity]),
+            'breed': random.choice(self.COMPLEXITY_TO_BREED[task.complexity]),
             'type': type(task).__name__,
             'to_kill': task.task,
             'data': task.data,
@@ -70,18 +82,8 @@ def spawn_wave(complexity, players_number, enemies):
         redis.set(f'enemy:{enemy["id"]}', enemy_encoded)
         redis.publish('game-commands', f'create {enemy["breed"]} {color}')
 
-    # spawn
-    for color in player_colors[:players_number]:
-        for _ in range(enemies):
-            _spawn_enemy(color)
-
-    # sustain
-    # while True:
-    #
-    #                 _spawn_enemy()
-    #     sleep(1)
-
-
-def clear_enemies_db():
-    for key in redis.scan_iter("enemy:*"):
-        redis.delete(key)
+    def clear_enemies_db(self):
+        for key in redis.scan_iter("enemy:*"):
+            enemy = EnemySchema().loads(redis.get(key))
+            redis.publish('game-commands', f'kill {enemy["breed"]} {enemy["color"]}')
+            redis.delete(key)
